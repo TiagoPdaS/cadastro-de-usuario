f41f109efbf3810eb0b58153e42d79866c095e3c
myuser <myemail>
2024-04-19 14:51:42 -0400

    add users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import People from "./assets/people.svg"
-import Arrow from "./assets/arrow.svg"
+import People from "./assets/people.svg";
+import Arrow from "./assets/arrow.svg";
 import {
   Container,
   H1,
@@ -10,24 +10,38 @@ import {
   Input,
   Button,
 } from "./styles";
+
 function App() {
+  const users = [
+    { id: Math.random(), name: "Iago", age: 33 },
+    { id: Math.random(), name: "Samuel", age: 11 },
+  ];
   return (
     <Container>
-        <Image alt="logo-people" src={People}/>
-        <ContainerItems>
+      <Image alt="logo-people" src={People} />
+      <ContainerItems>
         <H1>Hello!</H1>
 
         <InputLabel>Name</InputLabel>
-        <Input placeholder="Name"/>
-       
+        <Input placeholder="Name" />
+
         <InputLabel>Age</InputLabel>
-        <Input placeholder="Age"/>
+        <Input placeholder="Age" />
 
-        <Button>Submit<img alt="logo-arrow" src={Arrow}/> </Button>
+        <Button>
+          Submit
+          <img alt="logo-arrow" src={Arrow} />{" "}
+        </Button>
 
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.age}
+            </li>
+          ))}
+        </ul>
 
-        </ContainerItems>
-      
+      </ContainerItems>
     </Container>
   );
 }
